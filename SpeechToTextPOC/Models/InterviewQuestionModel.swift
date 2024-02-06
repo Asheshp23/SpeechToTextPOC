b94661f6a0b9b82af05e0ad7b2ab40a7baaad98b
asheshp23 <asheshp23@gmail.com>
2024-02-05 22:55:14 -0500

    Added speech to text transcriber and related views

---
+++ SpeechToTextPOC/Models/InterviewQuestionModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SpeechToTextPOC
+//
+//  Created by Ashesh Patel on 2024-02-05.
+//
+
+import Foundation
---
+++ SpeechToTextPOC/Views/QuestionAndAnswerView/QuestionAndAnswerView.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionAndAnswerView.swift
+//  SpeechToTextPOC
+//
+//  Created by Ashesh Patel on 2024-02-05.
+//
+
+import Foundation
