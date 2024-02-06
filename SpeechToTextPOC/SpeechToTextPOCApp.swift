e983788f3894618611644a7a798d20843d226755
asheshp23 <asheshp23@gmail.com>
2024-02-05 22:30:11 -0500

    Initial Commit

---
+++ SpeechToTextPOC/SpeechToTextPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextPOCApp.swift
+//  SpeechToTextPOC
+//
+//  Created by Ashesh Patel on 2024-02-05.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
